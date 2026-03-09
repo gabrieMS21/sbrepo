0c5f177ef88a54816464fbda0995fb94ebfe33ac
Paulo <prtcandido@hotmail.com>
2026-03-09 14:03:28 -0300

    errojavaversion

--- src/main/java/com/fatecpg/exemplopers/controller/PessoaController.java
+++ src/main/java/com/fatecpg/exemplopers/controller/PessoaController.java
@@ -9,16 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @RestController
 @RequestMapping("/meusite")
